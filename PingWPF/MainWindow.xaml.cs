dc653cef80b8d90bbbdb17bed28a110bf6593fb6
satosCZ <jk.satos@gmail.com>
2023-06-08 05:28:46 +0200

    Add files via upload
    First commit

---
+++ PingWPF/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PingWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private PingModel pingModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            StateChanged += MainWindowStateChangeRaised;
+            pingModel = new PingModel();
+            DataContext = pingModel;
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            string ipAddress = IPAddressTextBox.Text.Trim();
+            pingModel.StartPing(ipAddress);
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            pingModel.StopPing();
+        }
+
+        private void CommandBinding_CanExecute( object sender, CanExecuteRoutedEventArgs e )
+        {
+            e.CanExecute = true;
+        }
+
+        // Minimize
+        private void CommandBinding_Executed_Minimize(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.MinimizeWindow(this);
+        }
+
+        // Maximize
+        private void CommandBinding_Executed_Maximize( object sender, ExecutedRoutedEventArgs e )
+        {
+            SystemCommands.MaximizeWindow( this );
+        }
+
+        // Restore
+        private void CommandBinding_Executed_Restore( object sender, ExecutedRoutedEventArgs e )
+        {
+            SystemCommands.RestoreWindow( this );
+        }
+
+        // Close
+        private void CommandBinding_Executed_Close( object sender, ExecutedRoutedEventArgs e )
+        {
+            SystemCommands.CloseWindow( this );
+        }
+        
+        // State change
+        private void MainWindowStateChangeRaised(object sender, EventArgs e )
+        {
+            if (WindowState == WindowState.Maximized)
+            {
+                MainWindowBorder.BorderThickness = new Thickness( 8 );
+                RestoreButton.Visibility = Visibility.Visible;
+                MaximizeButton.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                MainWindowBorder.BorderThickness = new Thickness ( 0 );
+                RestoreButton.Visibility = Visibility.Collapsed;
+                MaximizeButton.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
---
+++ PingWPF/PingResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PingWPF
+{
+    public class PingResult
+    {
+        public DateTime Timestamp { get; }
+        public long RoundtripTime { get; }
+
+        public PingResult( DateTime timestamp, long roundtripTime )
+        {
+            Timestamp = timestamp;
+            RoundtripTime = roundtripTime;
+        }
+    }
+}
